bf00839e0752baa36a171c2a90accc8cb0970803
Sebastian <sebastian.cavada.lab@gmail.com>
2021-03-31 10:50:02 +0200

    🐛 som bugs fixed

--- routes/stocks/singleStock.js
+++ routes/stocks/singleStock.js
@@ -2,28 +2,24 @@ var pool = require('../../db/postgres').pool;
 
 module.exports = (req, res) => {
 
-  //import { pool } from ('../../db/postgres')
-
   pool.connect((err, client, release) => {
-
-    const query = {    
-      name: 'fetch-data',
-      text: 'SELECT * FROM past_values WHERE ticker = $1',
-      values: [ req.params.ticker.toUpperCase() ],
-    }
 
     if (err) {
       return console.error('Error acquiring client', err.stack)
     }
+
+    const query = {
+      name: 'fetch-past-values',
+      text: 'SELECT * FROM past_values WHERE ticker = $1',
+      values: [req.params.ticker.toUpperCase()],
+    }
+
     client.query(query, (err, result) => {
       release()
       if (err) {
         return console.error('Error executing query', err.stack)
-      }      
-
-      //var stock = req.params.ticker
-      res.status(200).json({ "ticker": result.rows });
-
+      }
+      res.status(200).json({ "past-values": result.rows });
     })
 
   })
