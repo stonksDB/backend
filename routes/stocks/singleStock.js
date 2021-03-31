1053abcef659a6652d1ffd9bb5a07968a515b3a0
Sebastian <sebastian.cavada.lab@gmail.com>
2021-03-31 10:18:24 +0200

    ✨ get data from db

--- routes/stocks/singleStock.js
+++ routes/stocks/singleStock.js
@@ -1,17 +1,21 @@
 var pool = require('../../db/postgres').pool;
 
-//import { pool } from ('../../db/postgres')
-
 module.exports = (req, res) => {
 
   //import { pool } from ('../../db/postgres')
 
   pool.connect((err, client, release) => {
 
+    const query = {    
+      name: 'fetch-data',
+      text: 'SELECT * FROM past_values WHERE ticker = $1',
+      values: [ req.params.ticker.toUpperCase() ],
+    }
+
     if (err) {
       return console.error('Error acquiring client', err.stack)
     }
-    client.query('SELECT NOW()', (err, result) => {
+    client.query(query, (err, result) => {
       release()
       if (err) {
         return console.error('Error executing query', err.stack)
