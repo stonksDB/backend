d97e69abedf3b01a6eaca75180eb7fabbef328a8
Sebastian <sebastian.cavada.lab@gmail.com>
2021-04-01 09:19:47 +0200

    finished pageable

--- routes/stocks/index.js
+++ routes/stocks/index.js
@@ -2,46 +2,53 @@ const stocks = require('express').Router();
 const { models } = require('../../sequelize');
 
 stocks.get("/:mic", getStocksByMic);
-stocks.get("/", getAllStocks)
+stocks.get("/", getStocksByMic)
 
 async function getStocksByMic(req, res) {
 
     var offset = req.query.offset ?? 0
     var limit = req.query.limit ?? 5
+    var mic = req.params.mic ?? "*"
+
+    const query = buildQuery(mic, offset, limit);
 
     try {
-        const stocks = await models.stock.findAll({
-            where: {
-                mic: req.params.mic,
+
+        const stocks = await models.stock.findAll(query);
+        const total = await models.stock.count();
+
+        const result = {
+            data: {
+                stocks: stocks
             },
-            order: [['ticker', 'ASC']],
-            offset: offset,
-            limit: limit
-        });
+            pageable: {
+                offset: offset,
+                limit: limit,
+                total: total
+            }
+        }
 
-        res.status(200).json(stocks);
+        res.status(200).json(result);
     } catch (error) {
         console.log("there was an error", error)
     }
 
 };
 
-async function getAllStocks(req, res) {
-
-    var offset = req.query.offset ?? 0
-    var limit = req.query.limit ?? 5
-
-    try {
-        const stocks = await models.stock.findAll({
-            order: [['ticker', 'ASC']],
-            offset: offset,
-            limit: limit
-        });
-        res.status(200).json(stocks);
-    } catch (error) {
-        console.log("there was an error", error)
+function buildQuery(mic, offset, limit) {
+    const query = {
+        order: [['ticker', 'ASC']],
+        offset: offset,
+        limit: limit
     }
 
-};
+    if (mic != "*") {
+        query.where = {
+            mic: mic,
+        }
+    }
+
+    return query;
+}
 
 module.exports = stocks;
