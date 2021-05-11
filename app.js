ff6fe58d9e4bb67498787a25acbd6fcae20ef4f1
Sebastian <sebastian.cavada.lab@gmail.com>
2021-05-11 18:38:17 +0200

    ✨ static site test

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const port = 8082
 
 const specs = swaggerJSDoc(swaggerDocument)
 
-app.use('/api/', routes)
+app.use('/api/', routes);
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/", express.static(__dirname + "/site"));
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
