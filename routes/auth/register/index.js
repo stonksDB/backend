ad4ee77dafcd7692b6220bb4ad083c800078a922
gavi210 <rccrd.rigoni@gmail.com>
2021-05-19 17:28:36 +0200

    save before changes

--- routes/auth/register/index.js
+++ routes/auth/register/index.js
@@ -13,6 +13,25 @@ ajv.addSchema(registrationSchema, 'new-user')
 const sequelize = require('../../../sequelize');
 const { stringify } = require('querystring');
 
+// dictionary - contains the list of valid sector names and their correspondent id
+const convertion_table = {
+  "Healthcare": 0,
+  "Industrials": 1,
+  "Consumer Cyclical": 2,
+  "Financial Services": 3,
+  "Technology": 4,
+  "Energy": 5,
+  "Real Estate": 6,
+  "Basic Materials": 7,
+  "Communication Services": 8,
+  "Consumer Defensive": 9,
+  "Utilities": 10,
+  "Industrial Goods": 11,
+  "Services": 12,
+  "Financial": 13, 
+  "Consumer Goods":14
+}
+
 /**
  * Checks if user already logged -> req.session.email != null or undefined
  */
@@ -95,23 +114,42 @@ let withEmail = (key) => {
   return sha256.update(password).digest('base64');
 }
 
+let followed_sectors_data = (body) => {
+  const { follows_arr } = body
+
+  // 1) filters out all not-valid sectors
+  // 2) valid sectors are stored in the toBeInserted array with their correspondent id
+  const toBeInserted = []
+  follows_arr.forEach(str => {
+    if(convertion_table[str] !== undefined) // valid sector
+      toBeInserted.push({
+        str: convertion_table[str]
+      }) 
+  })
+  console.log("output dictionary: " + stringify(toBeInserted));
+  return toBeInserted;
+}
+
 /**
  * 
  * @param {String} body body request from the user 
  * @param {Integer} nextIndex next index for the nex user
  * @returns 
  */
- let withData = (body, nextIndex) => {
-  const { firstName, lastName, dob, country, email, password } = body;
-  const dict = {
+ let share_holder_data = (body, nextIndex) => {
+   const { firstName, lastName, dob, country, email, password } = body;
+   const hashedPassword = getHashedPassword(password)
+   
+   const dict = {
     share_holder_id: nextIndex,
     first_name: firstName,
     last_name: lastName,
     dob: dob,
     country: country, 
     email: email,
-    password: getHashedPassword(password)
+    password: hashedPassword
   };
+  
   
   dict.additionalField = 0;
   return dict;
@@ -134,12 +172,19 @@ let withEmail = (key) => {
       // add the new user
       .then(last_inserted_share_holder => {
         // update db with new user
-        return sequelize
-          // exploit transaction - if error rollback
-          .transaction(function (t) {
-            return sequelize.models.share_holder
-              // create new user instance 
-              .create(withData(req.body, last_inserted_share_holder.share_holder_id + 1), {transaction: t});
+        return sequelize.transaction(function (t) {
+            const insert_user_data = sequelize.models.share_holder
+              .create(share_holder_data(req.body, last_inserted_share_holder.share_holder_id + 1), {transaction: t});
+            
+            const insert_followed_sectors = followed_sectors_data(req.body).forEach(sector => {
+              sequelize.models.sector
+              .create(sector, {transaction: t});
+            })
+            
+            return {
+              ...insert_user_data,
+              insert_followed_sectors
+            }
           })
           .then(function (result) {
             // transaction commit
@@ -152,6 +197,33 @@ let withEmail = (key) => {
       });
     };
  };
+
+ try {
+
+  const result = await sequelize.transaction(async (t) => {
+
+    const insert_user_data = await sequelize.models.share_holder.create(
+      share_holder_data(req.body, last_inserted_share_holder.share_holder_id + 1),
+      {transaction: t}
+    );
+
+    const insert_followed_sectors = await sequelize.models.sector.bulkCreate(
+      followed_sectors_data,
+      {transaction: t}
+    );
+
+    return {
+      ...insert_user_data,
+      insert_followed_sectors
+    };
+  });
+
+} catch (error) {
+
+  // If the execution reaches this line, an error occurred.
+  // The transaction has already been rolled back automatically by Sequelize
+  console.log("\n\nSomething went wrong ")
+}
 
 /**
  * Update current user session - add its email address in the user object
