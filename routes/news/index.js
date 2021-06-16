d73f7d53cdbe3ce8cf7e21f281b3ba94ffd8784d
Sebastian <sebastian.cavada.lab@gmail.com>
2021-06-16 15:36:19 +0200

    custom news coming soon

--- routes/news/index.js
+++ routes/news/index.js
@@ -5,52 +5,107 @@ const { Op } = require('sequelize');
 const fs = require('fs');
 
 const axios = require('axios')
-
-news.get("/:ticker", getNewsByTicker);
+news.get('/', getNewsPersonalized)
+news.get("/:ticker", getNewsByTickerRequestHandler);
 news.get("/single/:uuid", getSingleNewsByUuid);
 
-async function getNewsByTicker(req, res) {
+async function getNewsPersonalized(req, res) {
+
+    if (isUserLogged(req.session.user)) {
+
+        res.send("NEWS CUSTOM COMING SOON")
+        
+
+    } else {
+        
+        getMostSearchedTickerRedis().then(response => {
+            console.log(response)
+
+            let promisesList = []
+
+            let newsList = []
+
+            response.forEach(element => {
+
+                let news_ticker = getNewsByTicker(element, 2);
+                promisesList.push(news_ticker);
+            });
+
+            Promise.all(promisesList).then(news => {
+                newsList.push(news)
+                res.send(newsList)
+            })
+
+
+        })
+
+    }
+
+}
+
+function isUserLogged(user) {
+
+    var logged = user == undefined ? false : true;
+    return logged;
+}
+
+async function getMostSearchedTickerRedis() {
+    return Promise.resolve(["TSLA", "AAPL", "RACE", "BYND", "PLTR"])
+}
+
+async function getNewsByTickerRequestHandler(req, res) {
 
     const ticker = req.params.ticker;
     const number = req.query.number ?? 5;
 
-    console.log(number)
+    getNewsByTicker(ticker, number).then(news => {
+        return res.send(news)
+    }).catch(err => res.status(500).send(err))
 
-    var options = {
-        method: 'POST',
-        url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list',
-        params: { s: ticker, region: 'IT', snippetCount: number },
-        headers: {
-            'content-type': 'text/plain',
-            'x-rapidapi-key': 'Dzzd2zsPGXmshEw7W0fIiNYZklJZp1ebqsmjsnrFbX2oNhRmND',
-            'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
-        },
-    };
+}
 
-    axios.request(options).then(function (response) {
+async function getNewsByTicker(ticker, number) {
 
-        let results = []
+    return new Promise((resolve, reject) => {
 
-        let data = JSON.stringify(response.data.data.main.stream);
-        fs.writeFileSync('student-2.json', data);
+        console.log(number)
 
-        response.data.data.main.stream.forEach(element => {
-            const smallElem = {
-                "uuid": element.id,
-                "title": element.content.title,
-                "img": element.content.thumbnail.resolutions[0] ?? "",
-                "published_at": element.content.pubDate,
-                "provider": element.content.provider.displayName
-            }
+        var options = {
+            method: 'POST',
+            url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list',
+            params: { s: ticker, region: 'IT', snippetCount: number },
+            headers: {
+                'content-type': 'text/plain',
+                'x-rapidapi-key': 'Dzzd2zsPGXmshEw7W0fIiNYZklJZp1ebqsmjsnrFbX2oNhRmND',
+                'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+            },
+        };
 
-            results.push(smallElem)
+        axios.request(options).then(function (response) {
+
+            let results = []
+
+            response.data.data.main.stream.forEach(element => {
+                const smallElem = {
+                    "uuid": element.id,
+                    "title": element.content.title,
+                    "img": element.content.thumbnail.resolutions[0] ?? "",
+                    "published_at": element.content.pubDate,
+                    "provider": element.content.provider.displayName
+                }
+
+                results.push(smallElem)
+            });
+
+            console.log(results)
+
+            resolve(results);
+
+        }).catch(function (error) {
+            reject(error);
         });
 
-        res.send(results)
-
-    }).catch(function (error) {
-        console.error(error);
-    });
+    })
 
     //ERRORS
     // server does not respond
@@ -74,9 +129,9 @@ async function getSingleNewsByUuid(req, res) {
     };
 
     axios.request(options).then(function (response) {
-        res.send(response.data)        
+        res.send(response.data)
     }).catch(function (error) {
-        res.send(error)        
+        res.send(error)
     });
 
 };
