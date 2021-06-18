5e677ffdc55d39603e84d74fee13ff833181e5b3
gavi210 <rccrd.rigoni@gmail.com>
2021-06-18 16:03:03 +0200

    add: get info single company

--- routes/company/index.js
+++ routes/company/index.js
@@ -10,4 +10,20 @@ company.get("/all", (_, res) => {
       .catch(err => { return res.status(400).send(JSON.stringify(err))});
 });
 
+company.get("/single/:ticker", (req, res) => {
+   const ticker = req.params.ticker;
+
+   sequelize.models.company.findOne(
+      {
+         where:{
+            ticker: ticker
+         }
+      }).then(cmp => { 
+         if(!cmp) // no company matching the ticker provided
+            return res.status(400).send("Invalid Ticker");
+         else
+            return res.status(200).send(JSON.stringify(cmp)) 
+      }).catch(_ => { return res.status(500).send('Error on database read!') })
+})
+
 module.exports = company;
