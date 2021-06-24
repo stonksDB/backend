0bfeca00c4714380e2a0189008f40a2cc29b05b7
Sebastian <sebastian.cavada.lab@gmail.com>
2021-06-24 18:41:12 +0200

    ⚡️ redis interface better

--- routes/utils/redis/user_redis_utils.js
+++ routes/utils/redis/user_redis_utils.js
@@ -1,60 +1,64 @@
 const redis = require('redis');
+const config = require('./../../../config/config.json')
 
-// client for interaction
-const client = redis.createClient({
-    host: '25.68.176.166',
-    port: 6379,
-    db: 1
+const USER_ANALYTICS_DB = 2;
+
+// returns a redisClient instance to interact with the database
+exports.redisClient = redis.createClient({
+    host: config.host,
+    port: 6379, // default redis port
+    db: USER_ANALYTICS_DB
 });
-
-const empty_json_str = "{}";
 
 /**
  * 
- * @param {String} key - email associated to the logged user
+ * @param {String} user - email associated to the logged user
  * @returns - empty string if entry does not exist, JSON string if key matches
  */
-exports.getUserAnalytics = async (key) => {
-    console.log(key)
+exports.getUserAnalytics = async (user) => {
+
+    const mail = user;
 
     return new Promise((resolve, reject) => {
-        client.get(key, (error, value) => {
-            if (error) {
-                console.log(error)
-                reject(error)
-            }
-            if (value === null) { // no key present for the user
-                resolve({})
-            }
-            else // parse the data already present
-            {
-                let json = JSON.parse(value)                
-                resolve(Object.values(json))
-            }
-        })
+
+        const filteringArgs = ["+inf", "-inf", "LIMIT", "0", "5"]
+
+        redisClient.zrevrangebyscore(mail, filteringArgs, (err, res) => {
+            if (err)
+                reject(err)
+            else
+                resolve(res)
+        });
     });
 
 }
 
 /**
  * Given an email and a string, stores it to support analytics
- * @param {String} user - email of the user 
+ * @param {String} user - email associated to the logged user
  * @param {String} ticker - ticker for which to update the counter
  * @returns 
  */
 exports.updateTickerCounterUser = async (user, ticker) => {
 
-    this.getUserAnalytics(user).then(json_obj => {
-        if (json_obj[ticker]) {
-            let counter = json_obj[ticker]
-            json_obj[ticker] = counter + 1 // increment
-        }
-        else {
-            json_obj[ticker] = 1 // create new attribute
+    const mail = user;
+
+    return client.zscore(mail, ticker, (err, prevCounter) => {
+        if (err)
+            console.error(err) // log as error priority
+
+        let counter = 1;
+
+        if (prevCounter) { // ticker already in the set
+            counter = (parseInt(prevCounter) + 1)
         }
 
-        // store back value
-        client.set(user, JSON.stringify(json_obj));
-    }).catch(err => console.log("error", err))
+        const filteringArgs = [mail, counter, ticker]
+
+        client.zadd(filteringArgs, (err) => {
+            if (err)
+                console.error(err);
+        });
+    });
 
 }
