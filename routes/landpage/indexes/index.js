b3870778c9ada754b60361746eec0436c33ee1e0
Sebastian <sebastian.cavada.lab@gmail.com>
2021-06-27 21:06:49 +0200

    🐛 landpage indexes fixed

--- routes/landpage/indexes/index.js
+++ routes/landpage/indexes/index.js
@@ -7,25 +7,23 @@ const index_list_json = require('./indexes_list.json')
  * returns a JSON object in the format {'index' : '<index_prices_info>'}
  */
 indexes.get('/', (_, res) => {
-        
-    const index_list = JSON.parse(index_list_json)
 
     // list of Promise Response returned by the 'getHistoryTicker' function (NOT NEEDED)
     const pending_responses = []
 
-    indexes.forEach(index => {
+    index_list_json.forEach(index => {
         // '1d' since index data of interest only in the last day
         // '15m' more rare data - reduce size of response
         pending_responses.push(getHistoryTicker(index.ticker, '1d', "15m"))
     });
     
-    Promise.all(pending_responses).then(indexes_data => {
+    Promise.all(pending_responses).then(indexes_data => {        
 
         for(var i = 0; i < indexes.length; i++){
-            index_list[i]["points"] = indexes_data[i]
+            index_list_json[i]["points"] = indexes_data[i]
         }
         
-        res.status(200).json(indexes)
+        res.status(200).json(index_list_json)
 
     }).catch(err => { return res.status(500).send(err) })
 
