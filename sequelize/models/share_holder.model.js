3b21a5e9b46b68f399981e271efdab3572ff5351
gavi210 <rccrd.rigoni@gmail.com>
2021-06-28 21:20:04 +0200

    add shareholder model

---
+++ sequelize/models/share_holder.model.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    sequelize.define('share_holder', {
+
+        share_holder_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING(254),
+            allowNull: false,
+            primaryKey: true
+        },
+        password: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        first_name: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        dob : {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        country: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        }
+    }, {
+        tableName:"share_holders"
+    })
+}
